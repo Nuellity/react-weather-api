1812fc327efe47338e2f3fd809be7f5bcf653774
Nuellity <juxnuel@gmail.com>
2023-06-11 21:07:13 -0700

    first commit

---
+++ src/fetchWeatherData.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+const API_KEY = "dbca8aadd8f998d2fb67b6b8ff504fee";
+
+const makeIconURL = (iconId) =>
+  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  try {
+    const weatherInfo = await axios.get(URL);
+
+    const {
+      weather,
+      main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+      wind: { speed },
+      sys: { country },
+      name,
+    } = weatherInfo.data;
+
+    const { description, icon } = weather[0];
+
+    return {
+      description,
+      iconURL: makeIconURL(icon),
+      temp,
+      feels_like,
+      temp_min,
+      temp_max,
+      pressure,
+      humidity,
+      speed,
+      country,
+      name,
+    };
+  } catch (error) {}
+};
+
+export { getFormattedWeatherData };
